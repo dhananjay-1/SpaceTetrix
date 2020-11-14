9da1251b8880723333e78c27bd9d9609bc5b21f5
dhananjay <dhananjaydeswal11@gmail.com>
2020-11-15 04:20:05 +0530

    Level 1 refined, Level 2 added

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    // SerializeField makes the data member editable through inspector and non-editable through other scripts
+    // The value specified here for SerializeField is just the default value which will be shown on inspector
+    [SerializeField] float mainThrust = 100f;  // thrust for flying up
+    [SerializeField] float rcsThrust = 100f;   // thrust for rotation, rcs : rotation control system
+
     Rigidbody rigidbody;
     AudioSource audioSource;
 
@@ -21,6 +26,22 @@ public class Rocket : MonoBehaviour
         ProcessInput();
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Ok");
+                break;
+            case "Finish":
+                print("Finish");
+                break;
+            default:
+                print("dead");
+                break;
+        }
+    }
+
     private void ProcessInput()
     {
         Thrust();
@@ -31,7 +52,9 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up);
+            float flyingSpeed = mainThrust;
+                
+            rigidbody.AddRelativeForce(Vector3.up * flyingSpeed);
 
             if (!audioSource.isPlaying)
             {
@@ -49,13 +72,16 @@ public class Rocket : MonoBehaviour
     {
         rigidbody.freezeRotation = true; // Disabling the physics of rotation while taking manual control just to provide more stability
 
+        float rotationSpeed = rcsThrust * Time.deltaTime;  // Time.deltaTime gives the time for which previous frame remained and is a good estimator of current frame time span
+                                                           // If frame time span is less, the rotationSpeed is less and is more for longer frame time span, this ensures frame rate independence
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationSpeed);
         }
 
         rigidbody.freezeRotation = false;  // Enabling the physics of rotation again 
